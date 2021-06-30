24aae6217b09058dec9f0c1df59b0270c32ebb2c
Huanying04 <ken911230@gmail.com>
2021-06-30 11:06:17 +0800

    update

--- js/nav.js
+++ js/nav.js
@@ -1,19 +1,19 @@
 var path = window.location.pathname;
-var page = path.split("/").pop();
+var page = path.split("/").pop().split("\.")[0];
 console.log(page);
 
 switch (page) {
-    case "index.html":
+    case "index":
     default:
         document.getElementById('nav-home-btn').classList.add('active');
         break;
-    case "artworks.html":
+    case "artworks":
         document.getElementById('nav-artworks-btn').classList.add('active');
         break;
-    case "faith.html":
+    case "faith":
         document.getElementById('nav-faith-btn').classList.add('active');
         break;
-    case "about.html":
+    case "about":
         document.getElementById('nav-about-btn').classList.add('active');
         break;
 }
